2051215f42fb191cd91eb0d2714f2193e33b4c01
Yi <li.yi@intel.com>
2020-10-26 15:37:31 +0800

    remove useless

--- mask_detection.py
+++ mask_detection.py
@@ -1,12 +1,8 @@
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
-from imutils.video import VideoStream
 import numpy as np
-import imutils
-import time
 import cv2
-import os
 
 def detect_and_predict_mask(frame, faceNet, maskNet):
     # grab the dimensions of the frame and then construct a blob
